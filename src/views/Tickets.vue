5ef4588b1e6aa532e339a1954356137df9e29bef
Aleksei Gornev <vigadeparandus@gmail.com>
2022-03-02 21:44:18 +0200

    created Tickets.vue

--- src/views/Tickets.vue
+++ src/views/Tickets.vue
@@ -1,10 +1,64 @@
 <template>
-Siia tulevad pöördumised
+  <div>
+    <p> Siia tulevad pöördumised </p>
+<div>
+  <input placeholder="Pöördumise kirjeldus" v-model="ticket.description">
+  <input placeholder="Täitmise tähtaeg" v-model="ticket.dueDate">
+</div>
+    <div>
+      <table class="table table-hover">
+        <thead>
+        <tr>
+          <th>Ticket id</th>
+<!--          <th>Kasutaja</th>-->
+          <th>Sisestatud</th>
+          <th>Tähtaeg</th>
+          <th>Sisu</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="ticket in tickets">
+          <td>{{ ticket.id }}</td>
+<!--          <td>{{ ticket.userName }}</td>-->
+          <td>{{ ticket.description }}</td>
+          <td>{{ ticket.enteredDate }}</td>
+          <td>{{ ticket.dueDate }}</td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+
 </template>
 
 <script>
 export default {
-  name: "Tickets"
+  name: "Tickets",
+  data: function () {
+    return {
+      tickets: {
+        id: '',
+        userName: '',
+        description: '',
+        enteredDate: '',
+        dueDate: '',
+      }
+    }
+  },
+  methods: {
+    showTickets: function () {
+      this.$http.get("/helpdesk/new/ticket")
+          .then(response => {
+            this.tickets = response.data
+            console.log(response.data)
+          }).catch(error => {
+        console.log(error)
+      })
+    }
+  },
+  beforeMount() {
+    this.showTickets()
+  }
 }
 </script>
 
